c33c8279857a39f313b08a29b7eab1693e0d68b8
Aayan165 <160133042+Aayan165@users.noreply.github.com>
2024-03-01 02:24:05 +0500

    Add files via upload

---
+++ datatypes.ts
@@ -0,0 +1,33 @@
+// DATA TYPES (ASSIGNMENT)
+
+// STRING
+let place : string = "karachi";
+console.log(place)
+
+// NUMBERS
+let age : number = 17;
+console.log(age)
+
+// BOOLEAN
+let student : boolean = true;
+console.log(student)
+
+// NULL
+let nothing : null = null;
+console.log(nothing)
+
+// UNDEFINED
+let undefinedvalue : undefined = undefined;
+console.log(undefinedvalue)
+
+// SYMBOL
+let symbolexample = Symbol("symbols are used to create private or unique object properties.");
+let thing = {
+    [symbolexample]: 'value'
+};
+console.log(thing[symbolexample])
+
+// BIGINT
+let large = BigInt ("9007199254740991");
+console.log(large)
+
