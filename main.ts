cf2d43536c746c309d5839f600ba4800207e31af
Aayan165 <160133042+Aayan165@users.noreply.github.com>
2024-03-09 13:49:27 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+//Assignment - CASES
+
+// Camel case
+// In camel case the first letter of 2nd word is capital only.
+let myName : string = "Syed Aayan Mahmood";
+console.log(myName);
+
+// Snake case
+// In snake case teo words are differentiated by using underscore (_) between them.
+let my_name : string = "Syed Aayan Mahmood"
+console.log(my_name);
+
+// Pascal case
+// In pascal case the first letter of every word is capital.
+let MyName : string = "Syed Aayan Mahmood"
+console.log(MyName);
+
+// Assignment - Arrays
+
+let students = ["Aayan", "Anas", "Faizan", "Behroz" ];
+console.log(students);
+console.log(students [0]);
+
+let cities : string[] = ["Karachi", "Islamabad", "Lahore", "Quetta", "Peshawar"];
+console.log(cities);
+console.log(cities[0]);
+
+let oddNumbers: number[] = [1, 3, 5, 7, 9, 11, 13, 15,];
+console.log(oddNumbers);
+console.log(oddNumbers[3]);
+
+let coding : boolean[] = [true];
+console.log(coding);
