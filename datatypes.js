806662fca8fd41317cabe48b6fdeaef6ae0e69ba
Aayan165 <160133042+Aayan165@users.noreply.github.com>
2024-03-10 02:27:37 +0500

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+// DATA TYPES (ASSIGNMENT)
+var _a;
+// STRING
+var place = "karachi";
+console.log(place);
+// NUMBERS
+var age = 17;
+console.log(age);
+// BOOLEAN
+var student = true;
+console.log(student);
+// NULL
+var nothing = null;
+console.log(nothing);
+// UNDEFINED
+var undefinedvalue = undefined;
+console.log(undefinedvalue);
+// SYMBOL
+var symbolexample = Symbol("symbols are used to create private or unique object properties.");
+var thing = (_a = {},
+    _a[symbolexample] = 'value',
+    _a);
+console.log(thing[symbolexample]);
+// BIGINT
+var large = BigInt("9007199254740991");
+console.log(large);
