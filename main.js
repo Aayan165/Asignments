7a5d5068c0cd66bdfb1ab4ed7c781ca375248fe1
Aayan165 <160133042+Aayan165@users.noreply.github.com>
2024-03-10 02:27:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//Assignment - CASES
+// Camel case
+// In camel case the first letter of 2nd word is capital only.
+var myName = "Syed Aayan Mahmood";
+console.log(myName);
+// Snake case
+// In snake case teo words are differentiated by using underscore (_) between them.
+var my_name = "Syed Aayan Mahmood";
+console.log(my_name);
+// Pascal case
+// In pascal case the first letter of every word is capital.
+var MyName = "Syed Aayan Mahmood";
+console.log(MyName);
+// Assignment - Arrays and Objects
+var students = ["Aayan", "Anas", "Faizan", "Behroz"];
+console.log(students);
+console.log(students[0]);
+var cities = ["Karachi", "Islamabad", "Lahore", "Quetta", "Peshawar"];
+console.log(cities);
+console.log(cities[0]);
+var oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15,];
+console.log(oddNumbers);
+console.log(oddNumbers[3]);
+var coding = [true];
+console.log(coding);
+var Data = {
+    name: 'owais',
+    Class: 5,
+    vanue: "Governor House"
+};
+console.log(Data.name); // output owais
